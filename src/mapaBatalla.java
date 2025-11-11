bf30f7a00ed508f9370cfaa0bc632bc92644e86b
dami <damianponte@hotmail.es>
2025-11-11 10:03:42 +0100

    apartado de tablero de batalla pedido al usuario echo

---
+++ src/mapaBatalla.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class mapaBatalla {
+    public static void main (String[] ads)
+    {
+        Scanner lector = new Scanner(System.in);
+        System.out.println("¿De cuanto quieres que sea el tablero?");
+        int num=lector.nextInt();
+
+        //para el tamaño del tablero cojemos la variable "num" que es la que pedimos al usuario por teclado.
+
+        int tablero[][]=new int[num][num];
+        //pedimos al usuario q nos de un array
+        for(int i=0;i< num;i++){
+            for (int j=0;j<num;j++){
+                tablero[i][j]=0;
+
+            }
+
+        }
+        //mostramos tamaño del tablero de batalla.
+        System.out.println("el tamaño del tablero es de:" + num + " x " + num);
+        for(int i=0;i<num;i++){
+            for(int j=0;j<num;j++){
+                System.out.print(tablero[i][j] + " ");
+            }
+            //colocamos un salto vacio para que al pasar el primer for,el siguiente se imprima abajo y no adelante.
+            System.out.println(" ");
+        }
+
+    }
+}
